a299d5f82abc97dc0411f6c01328782d992b16c5
AbijuruT <abijuruyves@gmail.com>
2021-09-05 20:43:02 +0200

    Initial commit

---
+++ assets/js/thisess.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 0,
+  //   loop: true,
+  arrow: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+    },
+    300: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
